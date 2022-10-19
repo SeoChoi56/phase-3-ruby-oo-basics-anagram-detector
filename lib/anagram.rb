a6d0edeb1aa8ca28b286b8002637821f149ffce7
Seo Choi <seochoi56@gmail.com>
2022-10-18 20:06:39 -0400

    commit tree 1041c9ca0b705e50fcd7eb7633cbdf7d1b46588d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require "pry"
+
+class Anagram 
+    attr_reader :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match (array)
+        wordCheckArray = word.split.map(&:chars).flatten.sort
+        returnArray = []
+
+        array.map do |w|
+            if w.split.map(&:chars).flatten.sort == wordCheckArray 
+                returnArray.push(w)
+            end
+        end
+        return returnArray
+    end
+end
